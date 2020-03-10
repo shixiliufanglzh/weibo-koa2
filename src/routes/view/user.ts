c3c8cbccac806b70d4f0dd3bb214f5f4ce847987
Reed Li <shixiliufanglzh@gmail.com>
2020-03-10 22:46:46 +0800

    fix: lake the check of current login status

--- src/routes/view/user.ts
+++ src/routes/view/user.ts
@@ -7,11 +7,35 @@ import { ExtendedContext } from '../../utils/extends';
 const router = new Router();
 
 router.get('/login', async (ctx: ExtendedContext, next) => {
-    await ctx.render('login', {});
+    await ctx.render('login', getLoginInfo(ctx));
 });
 
 router.get('/register', async (ctx: ExtendedContext, next) => {
-    await ctx.render('register', {});
+    await ctx.render('register', getLoginInfo(ctx));
 });
+
+/**
+ * @param {ExtendedContext} ctx koa2 context
+ * @return {{
+ *     isLogin: boolean,
+ *     userName: string,
+ * }} { isLogin, userName }
+ */
+function getLoginInfo(ctx: ExtendedContext) {
+    let data: {
+        isLogin: boolean,
+        userName?: string,
+    } = {
+        isLogin: false,
+    };
+    const userInfo = ctx.session.userInfo;
+    if (userInfo) {
+        data = {
+            isLogin: true,
+            userName: userInfo.userName,
+        };
+    }
+    return data;
+}
 
 export default router;
