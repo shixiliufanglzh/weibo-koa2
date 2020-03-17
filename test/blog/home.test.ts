b9cac20750ea491a3f0d66a8e7cd1feea6b3314c
Reed Li <shixiliufanglzh@gmail.com>
2020-03-17 16:16:20 +0800

    feat: add test cases of square page

--- test/blog/home.test.ts
+++ test/blog/home.test.ts
@@ -69,10 +69,24 @@ test('test blog creation', async () => {
     expect(res.body.data.image).toBe(testBlog.image);
 })
 
-// test create blog
+// load first page of blogs in profile
 test('load first page of blogs in profile', async () => {
     const res = await server
         .get(`/api/profile/loadMore/${testUser.userName}/0`)
+        .set('cookie', COOKIE);
+    expect(res.body.errno).toBe(0);
+    const data = res.body.data;
+    expect(data).toHaveProperty('isEmpty');
+    expect(data).toHaveProperty('count');
+    expect(data).toHaveProperty('pageSize');
+    expect(data).toHaveProperty('pageIndex');
+    expect(data).toHaveProperty('blogList');
+})
+
+// load first page of blogs in square
+test('load first page of blogs in square', async () => {
+    const res = await server
+        .get(`/api/square/loadMore/0`)
         .set('cookie', COOKIE);
     expect(res.body.errno).toBe(0);
     const data = res.body.data;
